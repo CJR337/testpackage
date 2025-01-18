6c553f95905c82da5bbbb9856a7806b940a47437
CJR337 <cr883296@gmail.com>
2025-01-18 08:53:12 -0500

    Many package updates
    Multiple package updates

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Standard R cars data set
+#'
+#' This is the standard mtcars data set included with base R.
+#'
+#' @format ## 'chk'
+#' A data frame with 32 rows and 11 variables:
+#' \describe{
+#'   \item{mpg}{miles per gallon}
+#'   \item{cyl}{number of cylinders}
+#'   \item{disp}{engine displacement}
+#'   \item{hp}{engine horse power}
+#'   \item{drat}{drag racing power}
+#'   \item{wt}{weight of car}
+#'   \item{qsec}{quality per second}
+#'   \item{vs}{vital sign of car}
+#'   \item{am}{type of radio}
+#'   \item{gear}{number of gears}
+#'   \item{carb}{number of carburetors}
+#' }
+"chk"
+
+#' Fake data to test package
+#'
+#' This is fake data to test an R package.
+#'
+#' @format ## 'chk'
+#' A data frame with 1000 rows and 1 variable:
+#' \describe{
+#'   \item{my_pkg_data}{This is my package data}
+#' }
+"my_pkg_data"
